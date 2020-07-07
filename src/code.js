1defd042aeb5280aaf4d829496fcc244bd320cb5
I7RANK <franfgh2000@hotmail.com>
2020-07-06 19:09:14 -0500

    I add comments to all functions

--- src/code.js
+++ src/code.js
@@ -1,65 +1,76 @@
 var num_piezas;
 
-// --------- estos dos array -----------
-var piezas_nivel = ["","","","","fin","","","","","rec","curv","curv","","","rec",
-"rec","curv","rec","rec","curv","ini","","","",""];
-var estados_win = [0,0,0,0,1,0,0,0,0,1,4,1,0,0,1,1,3,2,2,2,3,0,0,0,0];
+// --------- algunos niveles -----------
+var piezas_nivel = ["", "", "", "", "fin", "", "", "", "", "rec", "curv", "curv", "", "", "rec",
+    "rec", "curv", "rec", "rec", "curv", "ini", "", "", "", ""
+];
+var estados_win = [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 4, 1, 0, 0, 1, 1, 3, 2, 2, 2, 3, 0, 0, 0, 0];
 
-/*piezas ["","","","","fin","","","","","rec","curv","curv-x2","","","rec",
-"rec","curv","rec","rec","curv","ini","","","",""];
-estados [0,0,0,0,1,0,0,0,0,1,4,1,0,0,1,1,3,2,2,2,3,0,0,0,0]; */
-// piezas [curv, cruz, rec, curv, , rec, curv, rec, curv, , rec, curv, rec, curv, fin, curv, rec, rec, curv-x2, curv, , , , ini,]
-// estados [4, 1, 2, 1, 0, 1, 4, 2, 2, 0, 1, 3, 2, 1, 1, 3, 2, 2, 1, 2, 0, 0, 0, 3, 0]
+// var piezas_nivel = ["", "", "", "", "fin", "", "", "", "", "rec", "curv", "curv-x2", "", "", "rec",
+//     "rec", "curv", "rec", "rec", "curv", "ini", "", "", "", ""
+// ];
+// var estados_win = [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 4, 1, 0, 0, 1, 1, 3, 2, 2, 2, 3, 0, 0, 0, 0];
 
-/* 
-rec, curv, cruz, fin, ini, curv_x2
-*/
+// var piezas_nivel = ["curv", "rec", "rec", "curv", , "rec", "curv", "rec", "curv", , "rec", "curv", "rec",
+//     "curv", "fin", "curv", "rec", "rec", "curv-x2", "curv", , , , "ini",
+// ];
+// var estados_win = [4, 2, 2, 1, 0, 1, 4, 2, 2, 0, 1, 3, 2, 1, 1, 3, 2, 2, 1, 2, 0, 0, 0, 3, 0];
 
-// Nivel 2
-// curv, rec, rec, curv, , curv, rec, fin, curv, curv, , , , , rec, , , curv, rec, curv, , , ini, , 
-// 4, 2, 2, 1, 0, 3, 2, 2, 3, 1, 0, 0, 0, 0, 1, 0, 0, 4, 2, 2, 0, 0, 3, 0, 0
+/* ----------------------------------------------------------- */
+
 var estado_piezas = [0];
 var grados_piezas = [0];
 
+/**
+ * def_ruta - define la ruta de la imagen que se va a agregar
+ * @param {Array} p_n array de las piezas del nivel
+ * @param {Int} n indice para buscar la ruta de la img
+ */
 function def_ruta(p_n, n) {
     var ruta = "";
-    if (p_n[n]=="ini") {
+    if (p_n[n] == "ini") {
         ruta = "img/ini_a.png"
-    }else if (p_n[n]=="fin") {
+    } else if (p_n[n] == "fin") {
         ruta = "img/fin_a.png"
-    }else if (p_n[n]=="rec") {
+    } else if (p_n[n] == "rec") {
         ruta = "img/rec_b.png"
-    }else if (p_n[n]=="curv") {
+    } else if (p_n[n] == "curv") {
         ruta = "img/curv_b.png"
-    }else if (p_n[n]=="cruz") {
+    } else if (p_n[n] == "cruz") {
         ruta = "img/cruz_b.png"
-    }else if (p_n[n]=="curv-x2") {
+    } else if (p_n[n] == "curv-x2") {
         ruta = "img/curv-x2_b.png"
-    }else if (p_n[n]=="") {
+    } else if (p_n[n] == "") {
         ruta = "img/"
-    }else if (p_n[n]=="") {
+    } else if (p_n[n] == "") {
         ruta = "img/"
-    }else if (p_n[n]=="") {
+    } else if (p_n[n] == "") {
         ruta = "img/"
-    }else if (p_n[n]=="") {
+    } else if (p_n[n] == "") {
         ruta = "img/"
-    }else if (p_n[n]=="") {
+    } else if (p_n[n] == "") {
         ruta = "img/"
-    }else if (p_n[n]=="") {
+    } else if (p_n[n] == "") {
         ruta = "img/"
-    }else {
+    } else {
 
     }
     return ruta;
 }
 
+/**
+ * add_piezas - agraga las piezas y crea el tablero
+ * @param {Integer} num es el numero de casillas en el tablero
+ * @param {Boolean} edit si es true indica que esta en modo editor 
+ * sino es modo de juego
+ */
 function add_piezas(num, edit) {
     num_piezas = num;
     for (var i = 0; i <= num - 1; i++) {
         var divcontend = document.createElement("div");
         var divcontend_img = document.createElement("div");
         var imgpieza = document.createElement("img");
-        
+
         divcontend.setAttribute("id", "divcontend" + i);
         divcontend.setAttribute("Class", "contend_5x5");
         divcontend_img.setAttribute("Class", "contend_img_5x5");
@@ -77,7 +88,7 @@ function add_piezas(num, edit) {
 
         if (piezas_nivel[i] == "" || piezas_nivel[i] == null) {
 
-        }else {
+        } else {
             imgpieza.setAttribute("src", def_ruta(piezas_nivel, i));
             imgpieza.setAttribute("onclick", "rotar(this.id)");
             estado_piezas[i] = 1;
@@ -86,17 +97,17 @@ function add_piezas(num, edit) {
                 estado_piezas[i] = estados_win[i];
                 if (estados_win[i] == 1) {
                     imgpieza.style = "transform: rotate(0deg);";
-                }else if (estados_win[i] == 2) {
+                } else if (estados_win[i] == 2) {
                     imgpieza.style = "transform: rotate(90deg);";
-                }else if (estados_win[i] == 3) {
+                } else if (estados_win[i] == 3) {
                     imgpieza.style = "transform: rotate(180deg);";
-                }else if (estados_win[i] == 4) {
+                } else if (estados_win[i] == 4) {
                     imgpieza.style = "transform: rotate(270deg);";
                 }
             }
             if (edit == true) {
 
-            }else {
+            } else {
                 divcontend_img.appendChild(imgpieza);
             }
         }
@@ -104,16 +115,21 @@ function add_piezas(num, edit) {
 
         if (edit == true) {
             document.getElementById("div_editor").appendChild(divcontend);
-        }else {
+        } else {
             document.getElementById("div_juego").appendChild(divcontend);
         }
     }
 }
 
+/**
+ * saca_nom - obtiene el nombre estandar de la ruta de la pieza
+ * @param {Element} source es la pieza del tablero
+ */
 function saca_nom(source) {
     var nom_;
-    // este for saca el trozo del nombre de la imagen de la url del img LLLL
-    for (i = 0; i<= source.length - 1; i++) {
+    /* este for saca el trozo del nombre de la imagen de la url del img
+    (muchas eles en esta frace) */
+    for (i = 0; i <= source.length - 1; i++) {
         if (source.charAt(i) == "/") {
             nom_ = source.substring(i + 1, source.length);
         }
@@ -123,7 +139,7 @@ function saca_nom(source) {
     nom_ = nom_.substring(0, nom_.length - 4);
 
     // y aqui se quita lo que esta despues de el gion bajo
-    for (i = 0; i<= nom_.length - 1; i++) {
+    for (i = 0; i <= nom_.length - 1; i++) {
         if (nom_.charAt(i) == "_") {
             nom_ = nom_.substring(0, i);
         }
@@ -131,18 +147,23 @@ function saca_nom(source) {
     return nom_;
 }
 
+/**
+ * rotar - rota la pieza a la que se le hace click en el tablero
+ * @param {Element} id es la id de la pieza que se va a rotar
+ */
 function rotar(id) {
     var source = document.getElementById(id).src;
     var nom_img = "";
 
     nom_img = saca_nom(source);
-    // cambia algunos estados(las que no necesitan todos los estados) y reinicia estados
-    // el cruzado tiene un estado de 0
+    /* cambia algunos estados(las que no necesitan todos los estados)
+    y reinicia estados
+    el cruzado tiene un estado de 0 */
 
     /* estados: 
-    cruz_a y cruz_b = 1
-    cruz_ab y cruz_ba y curv_x2_a y curv_x2_b y rec_a y rec_b = 2
-    fin_a y ini_a y curv_a y curv_b = 4
+    cruz_a, cruz_b = 1
+    cruz_ab, cruz_ba, curv_x2_a, curv_x2_b, rec_a, rec_b = 2
+    fin_a, ini_a, curv_a, curv_b = 4
     */
 
     /* estandarizar nombres de estados
@@ -161,13 +182,13 @@ function rotar(id) {
     // el estado se asigna cuando se crean las piezas
     if (nom_img == "cruz") {
         estado_piezas[id] = 1;
-    }else if (nom_img == "ccruz" || nom_img == "curv-x2" || nom_img == "rec") {
-        if (estado_piezas[id] == 1){
+    } else if (nom_img == "ccruz" || nom_img == "curv-x2" || nom_img == "rec") {
+        if (estado_piezas[id] == 1) {
             estado_piezas[id] = 2;
-        }else if (estado_piezas[id] == 2) {
+        } else if (estado_piezas[id] == 2) {
             estado_piezas[id] = 1;
         }
-    }else if (nom_img == "fin" || nom_img == "ini" || nom_img == "curv") {
+    } else if (nom_img == "fin" || nom_img == "ini" || nom_img == "curv") {
         if (estado_piezas[id] == 4) {
             estado_piezas[id] = 0;
         }
@@ -178,7 +199,7 @@ function rotar(id) {
     grados_piezas[id] = grados_piezas[id] + 90;
     document.getElementById(id).style = "transform: rotate(" + grados_piezas[id] + "deg); transition: 0.4s;";
 
-    // para ganar
+    // este codigo verifica despues de cada click si ganas
     var np = 25;
     var ganas = 0;
     for (a = 0; a <= np - 1; a++) {
@@ -187,13 +208,12 @@ function rotar(id) {
         }
     }
     if (ganas == np) {
-        // alert("Maravilloso como el oso");
         document.getElementById("div_tit_juego").style = "top: 10px;";
-        for (e = 0; e<= 24; e++) {
+        for (e = 0; e <= 24; e++) {
             var elimg = document.getElementById(e);
-            if (elimg == null) {
-            }else {
+            if (elimg == null) {} else {
                 var url = elimg.src;
+                // no recuerdo para que puse el siguiente comentario :v
                 // falta el ab.png o el ba.png
                 var new_url = url.substring(0, url.length - 5) + "a.png";
                 elimg.setAttribute("src", new_url);
@@ -208,11 +228,19 @@ var div_contendimg = "";
 var new_idimg = "";
 var gene_limg = [""];
 var gene_estados = [0];
+
+// llena los array con los valores por defecto ("" y 0)
 for (var i = 0; i <= 24; i++) {
     gene_limg[i] = "";
     gene_estados[i] = 0;
 }
 
+/**
+ * generar - genera el codigo de un nivel en dos lineas
+ * la primera genera el nombre de las piezas
+ * y la segunda genera los estados
+ * ambas lineas separadas por comas en orden
+ */
 function generar() {
     var line = "";
     var esta = "";
@@ -226,23 +254,32 @@ function generar() {
     console.log(esta);
 }
 
+/**
+ * in_select - agrega una sombra a la casilla que se ha seleccionado
+ * para indicar que se puede agragar una ficha
+ * @param {Element} id es el elemento al que se le agragra o quitara la sombra
+ */
 function in_select(id) {
     div_contend.style = "none";
     if (div_contend != "") {
         if (div_contend != id) {
             div_contend = id;
             div_contend.style = "box-shadow: inset 0px 0px 20px #000;";
-        }else {
+        } else {
             div_contend.style = "none";
             div_contend = "";
         }
-    }else {
+    } else {
         div_contend = id;
         div_contend.style = "box-shadow: inset 0px 0px 20px #000;";
     }
     tituloo.innerText = "";
 }
 
+/**
+ * id_divcontend_img - obtiene la id de la casilla y se la asigna a @new_idimg 
+ * @param {Element} id es la casilla a la que se le sacara la id
+ */
 function id_divcontend_img(id) {
     var cortar = "";
     if (div_contendimg != "") {
@@ -250,22 +287,26 @@ function id_divcontend_img(id) {
             div_contendimg = id;
             cortar = div_contendimg.id;
             new_idimg = cortar.substring(14, div_contendimg.id.length);
-        }else {
+        } else {
             div_contendimg = "";
             new_idimg = undefined;
         }
-    }else {
+    } else {
         div_contendimg = id;
         cortar = div_contendimg.id;
         new_idimg = cortar.substring(14, div_contendimg.id.length);
     }
 }
 
-
+/**
+ * chosen - esta funcion agrega la pieza a la casilla que se selecciono
+ * las piezas son las que estan a la derecha en el modo editor
+ * @param {Element} id es la id de la pieza seleccionada
+ */
 function chosen(id) {
     if (div_contend == "" || div_contendimg == "" || new_idimg == "") {
         alert("Seleccione primero una casilla");
-    }else {
+    } else {
         var img_new = document.createElement("img");
         img_new.setAttribute("id", new_idimg);
         img_new.setAttribute("Class", "img_5x5");
@@ -278,7 +319,7 @@ function chosen(id) {
         img_new.setAttribute("onclick", "rotar(this.id)");
 
         div_contendimg.appendChild(img_new);
- 
+
         gene_limg[new_idimg] = saca_nom(id.src);
         gene_estados[new_idimg] = 1;
 
@@ -289,27 +330,37 @@ function chosen(id) {
     }
 }
 
+/**
+ * borrar - elimina la pieza en caso de que se active la funcion borrar
+ * @param {Element} id es el elemento que se quiere borrar
+ * @param {boolean} solob si es false se limpian los arrays
+ * ver funcion generar
+ */
 function borrar(id, solob) {
     var node = document.getElementById(id.id);
     node.parentNode.removeChild(node);
     if (solob == false) {
         gene_limg[id.id] = "";
         gene_estados[id.id] = 0;
-    }else {
-
     }
 }
 
 var tituloo = document.getElementById("titulo");
 var spann_t = document.getElementById("borrar_t");
 var spann_f = document.getElementById("borrar_f");
+
+/**
+ * cambio - esta funcion cambia el estado de borrar piezas y agragar
+ * @param {*} equelan 
+ * @param {*} id 
+ */
 function cambio(equelan, id) {
-    
+
     if (div_contend == "" || div_contendimg == "" || new_idimg == "") {
         if (equelan == true) {
             console.log(equelan);
             tituloo.innerText = "Eliminando";
-            for (i = 0; i <= num_piezas - 1; i++){
+            for (i = 0; i <= num_piezas - 1; i++) {
                 var div1 = document.getElementById("divcontend" + i);
                 var div2 = document.getElementById("divcontend_img" + i);
                 var img1 = document.getElementById(i);
@@ -321,16 +372,16 @@ function cambio(equelan, id) {
             }
             spann_t.setAttribute("class", "sombra icon-ex ocultar");
             spann_f.setAttribute("class", "sombra icon-chulo");
-        }else {
+        } else {
             console.log(equelan);
             titulo.innerText = "";
-            for (i = 0; i <= num_piezas - 1; i++){
+            for (i = 0; i <= num_piezas - 1; i++) {
                 var div1 = document.getElementById("divcontend" + i);
                 var div2 = document.getElementById("divcontend_img" + i);
                 var img1 = document.getElementById(i);
                 if (img1 != null) {
                     img1.setAttribute("onclick", "rotar(this.id)");
-                }else {
+                } else {
                     div1.setAttribute("onclick", "in_select(this);");
                     div2.setAttribute("onclick", "id_divcontend_img(this);");
                 }
@@ -338,13 +389,16 @@ function cambio(equelan, id) {
             spann_f.setAttribute("class", "sombra icon-chulo ocultar");
             spann_t.setAttribute("class", "sombra icon-ex");
         }
-        
-    }else {
+
+    } else {
         tituloo.innerText = "Desseleccione la casilla primero";
     }
 
 }
 
+/**
+ * recargar - devuelve el modo editor al estado inicial
+ */
 function recargar() {
     for (i = 0; i <= num_piezas - 1; i++) {
         var borrar_divcontend = document.getElementById("divcontend" + i);
@@ -363,19 +417,15 @@ function recargar() {
         gene_estados[i] = 0;
     }
     add_piezas(num_piezas, true);
-    
+
     // document.getElementById("div_editor").style = "animation: none;";
     // document.getElementById("div_editor").style = "animation: pulzo 1s;";
 }
 
-function develop() {
-    alert("Developing");
-}
-function consola() {
-    console.log("Developing");
-}
-
 // --------------------------- niveles ----------
+/**
+ * cargar_dlvls - carga el tablero en la pestaña niveles
+ */
 function cargar_dlvls() {
     for (i = 0; i <= 24; i++) {
         var div_lvls = document.createElement("div");
@@ -396,46 +446,73 @@ function cargar_dlvls() {
     }
 }
 
-function divs_hover (id) {
+/**
+ * divs_hover - cambia la animacion en los botones de niveles
+ * cuando el mouse pasa por encima
+ * @param {Element} id id del elemento
+ */
+function divs_hover(id) {
     var elemento = document.getElementById(id.id);
     elemento.style = "background: #0c0c0c; border-bottom: solid 3px #fff;";
 }
 
-function divs_out (id) {
+/**
+ * divs_out - cambia la animacion en los botones de niveles
+ * cuando el mouse se quita de encima
+ * @param {Element} id id del elemnto
+ */
+function divs_out(id) {
     var elemento = document.getElementById(id.id);
     elemento.style = "background: #000; border-bottom: solid 3px #0c4071;";
 }
 
-function c_color(ns,nl) {
-	if (nl == 1) {
-		document.getElementById("span_up" + ns).style.background = "linear-gradient(to right,#2b0016,#ff1751)";
-		document.getElementById("span_rg" + ns).style.background = "linear-gradient(to bottom,#2b0016,#ff1751)";
-		document.getElementById("span_dw" + ns).style.background = "linear-gradient(to left,#2b0016,#ff1751)";
-		document.getElementById("span_lf" + ns).style.background = "linear-gradient(to top,#2b0016,#ff1751)";
-	} else if (nl == 2) {
-		document.getElementById("span_up" + ns).style.background = "linear-gradient(to right,#000f2b,#1742ff)";
-		document.getElementById("span_rg" + ns).style.background = "linear-gradient(to bottom,#000f2b,#1742ff)";
-		document.getElementById("span_dw" + ns).style.background = "linear-gradient(to left,#000f2b,#1742ff)";
-		document.getElementById("span_lf" + ns).style.background = "linear-gradient(to top,#000f2b,#1742ff)";
-	}
-	
+/**
+ * c_color - cambia el color del borde de los botones del index
+ * @param {Int} ns numero del span al que se le cambiara el color
+ * (el primero o el segundo)
+ * @param {Int} nl numero que indicara que color poner
+ */
+function c_color(ns, nl) {
+    if (nl == 1) {
+        document.getElementById("span_up" + ns).style.background = "linear-gradient(to right,#2b0016,#ff1751)";
+        document.getElementById("span_rg" + ns).style.background = "linear-gradient(to bottom,#2b0016,#ff1751)";
+        document.getElementById("span_dw" + ns).style.background = "linear-gradient(to left,#2b0016,#ff1751)";
+        document.getElementById("span_lf" + ns).style.background = "linear-gradient(to top,#2b0016,#ff1751)";
+    } else if (nl == 2) {
+        document.getElementById("span_up" + ns).style.background = "linear-gradient(to right,#000f2b,#1742ff)";
+        document.getElementById("span_rg" + ns).style.background = "linear-gradient(to bottom,#000f2b,#1742ff)";
+        document.getElementById("span_dw" + ns).style.background = "linear-gradient(to left,#000f2b,#1742ff)";
+        document.getElementById("span_lf" + ns).style.background = "linear-gradient(to top,#000f2b,#1742ff)";
+    }
+
 }
 
+/**
+ * c_line - llama a c_color y cambia el color del texto cuando
+ * el mouse se coloca por encima (over)
+ * @param {Element} tipo elemento de donde se llamo esta funcion
+ */
 function c_line(tipo) {
-	if (tipo.id == "btn_j") {
-		c_color(1,1);
-		document.getElementById("btn_j").style.color = "#ff1751";
-	} else if (tipo.id == "btn_p") {
-		c_color(2,1);
-		document.getElementById("btn_p").style.color = "#ff1751";
-	}
+    if (tipo.id == "btn_j") {
+        c_color(1, 1);
+        document.getElementById("btn_j").style.color = "#ff1751";
+    } else if (tipo.id == "btn_p") {
+        c_color(2, 1);
+        document.getElementById("btn_p").style.color = "#ff1751";
+    }
 }
+
+/**
+ * c_lineout - llama a c_color y cambia el color del texto cuando
+ * el mouse se quita de encima (out)
+ * @param {*} tipo 
+ */
 function c_lineout(tipo) {
-	if (tipo.id == "btn_j") {
-		c_color(1,2);
-		document.getElementById("btn_j").style.color = "#1742ff";
-	} else if (tipo.id == "btn_p") {
-		c_color(2,2);
-		document.getElementById("btn_p").style.color = "#1742ff";
-	}
+    if (tipo.id == "btn_j") {
+        c_color(1, 2);
+        document.getElementById("btn_j").style.color = "#1742ff";
+    } else if (tipo.id == "btn_p") {
+        c_color(2, 2);
+        document.getElementById("btn_p").style.color = "#1742ff";
+    }
 }
